3d919322d9a1ffb75bb608f46d3a3623a3a60ad3
Maria Fredriksson <mf223wk@student.lnu.se>
2023-01-24 09:56:10 +0100

    Add boilerplate code to server.js and router.js + add some more basic files

---
+++ src/routes/router.js
@@ -0,0 +1,24 @@
+/**
+ * The routes.
+ *
+ * @author Maria Fredriksson & Mats Loock
+ * @version 1.0.0
+ */
+
+import express from 'express'
+import { router as homeRouter } from './home-router.js'
+import { router as snippetsRouter } from './snippets-router.js'
+
+export const router = express.Router()
+
+router.use('/', homeRouter)
+router.use('/snippets', snippetsRouter)
+
+//* If any url is requested that doesn't match the above, there will be an error
+router.use('*', (req, res, next) => {
+  const error = new Error('Not Found')
+  error.status = 404
+
+  //* Sends the errror to the the next middleware, which will trigger the error handler
+  next(error)
+})
