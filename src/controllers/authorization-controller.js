cda11c4b988c944980f05d33916f5fdc45360c3c
Maria Fredriksson <mf223wk@student.lnu.se>
2023-02-04 14:38:20 +0100

    Create a controller for authorization and move everything concerning that there

---
+++ src/controllers/authorization-controller.js
@@ -0,0 +1,59 @@
+/**
+ * Module for the AuthorizationController.
+ *
+ * @author Maria Fredriksson
+ * @version 1.0.0
+ */
+
+import { Snippet } from '../models/snippet.js'
+
+/**
+ * Encapsulates a controller.
+ */
+export class AuthorizationController {
+  /**
+   * Middleware to check if the user is authorized to access the resource.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   *
+   * @returns {undefined}
+   */
+  async authorizeLoggedin (req, res, next) {
+    // If the user is not logged in
+    if (!req.session.user) {
+      const error = new Error('Forbidden')
+      error.status = 403
+      return next(error)
+    }
+
+    // If the user is logged in
+    next()
+  }
+
+  /**
+   * Middleware to check if the user is authorized to access the resource.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   *
+   * @returns {undefined}
+   */
+  async authorizeUser (req, res, next) {
+    try {
+      const snippet = await Snippet.findById(req.params.id)
+
+      // If the user is not the author
+      if (req.session.user !== snippet.author) {
+        const error = new Error('Forbidden')
+        error.status = 403
+        return next(error)
+      }
+      next()
+    } catch (error) {
+      next(error)
+    }
+  }
+}
--- src/routes/snippets-router.js
+++ src/routes/snippets-router.js
@@ -7,20 +7,23 @@
 
 import express from 'express'
 import { SnippetsController } from '../controllers/snippets-controller.js'
+import { AuthorizationController } from '../controllers/authorization-controller.js'
 
 export const router = express.Router()
 
 const controller = new SnippetsController()
 
+const authorizationController = new AuthorizationController()
+
 // Map HTTP verbs and route paths to controller action methods.
 
 router.get('/', (req, res, next) => controller.index(req, res, next))
 
-router.get('/create', controller.authorizeLoggedin, controller.create)
-router.post('/create', controller.authorizeLoggedin, controller.createPost)
+router.get('/create', authorizationController.authorizeLoggedin, controller.create)
+router.post('/create', authorizationController.authorizeLoggedin, controller.createPost)
 
-router.get('/:id/update', controller.authorizeUser, controller.update)
-router.post('/:id/update', controller.authorizeUser, controller.updatePost)
+router.get('/:id/update', authorizationController.authorizeUser, controller.update)
+router.post('/:id/update', authorizationController.authorizeUser, controller.updatePost)
 
-router.get('/:id/delete', controller.authorizeUser, controller.delete)
-router.post('/:id/delete', controller.authorizeUser, controller.deletePost)
+router.get('/:id/delete', authorizationController.authorizeUser, controller.delete)
+router.post('/:id/delete', authorizationController.authorizeUser, controller.deletePost)
